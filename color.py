0658838f2df2e6a75ce105407f081fe5ac2df251
mimouncadosch <mimouncadosch@gmail.com>
2015-03-14 17:11:27 -0400

    Started writeup

--- color.py
+++ color.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from scipy.signal import argrelextrema
 from mpl_toolkits.mplot3d import Axes3D
 from histogram import histogram
 import timeit
@@ -12,7 +13,6 @@ idx2 	: index of second image
 b 		: number of bins
 b_lb    : the lower bound of what is considered a "black" pixel
 """
-
 
 def color_similarity(idx1, idx2, b, black_lb):
     # Parse ids to be consistent with image names
@@ -34,6 +34,8 @@ def color_similarity(idx1, idx2, b, black_lb):
 
     pixels = 89 * 60
 
+
+
     # L1 distance
     D = abs(hist2 - hist1)
     d_l1 = np.sum(D) / (2 * pixels)
@@ -45,9 +47,15 @@ def color_similarity(idx1, idx2, b, black_lb):
 
 
 if __name__ == "__main__":
-    bins_sims = []
+    # bins_sims_1 = []
+    # bins_sims_2 = []
+    # bins_sims_3 = []
+    # bins_sims_4 = []
+    # bins_sims_5 = []
+    # bins_sims_6 = []
+    # diffs = []
     blb_sims = []
-    runtimes = []
+    # runtimes = []
     # fig = plt.figure()
     # ax = fig.add_subplot(111, projection='3d')
 
@@ -57,6 +65,16 @@ if __name__ == "__main__":
 
 
 
+    for lb in np.arange(0, 40, 1):
+        similarity1 = color_similarity(5, 6, 14, lb)
+        blb_sims.append(similarity1)
+
+    plt.xlabel("Black Pixel Lower Bound")
+    plt.ylabel("Similarity")
+    plt.plot(blb_sims)
+    plt.show()
+
+    # color_similarity(5,6,32,10)
 
     # x = []
     # y = []
@@ -80,27 +98,134 @@ if __name__ == "__main__":
 
 
     # # Testing for different nbins values
-    # # for s in np.arange(1, 256, 10):
-    # #     # print s
-    # #     start = timeit.default_timer()
-    # #     similarity = (color_similarity(5, 6, s, 0))
-    # #     stop = timeit.default_timer()
-    # #     # print stop - start
-    # #     runtimes.append(stop-start)
-    # #     bins_sims.append(similarity)
-    # # plt.plot((sims))
-    # # plt.plot((runtimes))
-    # # plt.plot(np.diff(sims))
-    # # plt.plot(np.diff(runtimes))
+    # for b in np.arange(1, 20, 1):
+    #
+    #     # start = timeit.default_timer()
+    #     similarity_1 = (color_similarity(3, 4, b, 0))
+    #     similarity_2 = (color_similarity(5, 6, b, 0))
+    #     similarity_3 = (color_similarity(13, 14, b, 0))
+    #     similarity_4 = (color_similarity(17, 18, b, 0))
+    #     similarity_5 = (color_similarity(7, 9, b, 0))
+    #     similarity_6 = (color_similarity(29, 30, b, 0))
+    #     diff2 = similarity_1 - similarity_2
+    #     diff3 = similarity_6 - similarity_2
+    #     diff4 = similarity_6 - similarity_3
+    #     diff5 = similarity_6 - similarity_4
+    #     diff6 = similarity_6 - similarity_5
+    #     # stop = timeit.default_timer()
+    #     # print stop - start
+    #     # runtimes.append(stop-start)
+    #
+    #     bins_sims_1.append((diff2))
+    #     bins_sims_2.append((diff3))
+    #     bins_sims_3.append((diff4))
+    #     bins_sims_4.append((diff5))
+    #     bins_sims_5.append((diff6))
+    #     # bins_sims_6.append((similarity_6))
+    #     # diffs.append(diff)
+    #
+    #
+    # fig, ax = plt.subplots()
+    #
+    # g1 = "similarity(B) - similarity(A)"
+    # g2 = "similarity(B) - similarity(C)"
+    # g3 = "similarity(B) - similarity(D)"
+    # g4 = "similarity(B) - similarity(E)"
+    # g5 = "similarity(B) - similarity(F)"
+    # # g6 = "F"
+    # ax.plot((bins_sims_1), label=g1)
+    # ax.plot((bins_sims_2), label=g2)
+    # ax.plot((bins_sims_3), label=g3)
+    # ax.plot((bins_sims_4), label=g4)
+    # ax.plot((bins_sims_5), label=g5)
+    # ax.plot((bins_sims_6), label="Similarity(" + g6 + ")")
+
+    # ax.plot(diffs, label="Similarity (" + g1 + ") - Similarity(" + g2 + ")")
+
+    # legend = ax.legend(loc='upper right', shadow=False)
+    #
+    # frame = legend.get_frame()
+    # frame.set_facecolor('1.0')
+    #
+    # plt.xlabel("Number of Bins")
+    # plt.ylabel("Similarity")
+    #
+    # for label in legend.get_texts():
+    #     label.set_fontsize('large')
+    #
+    # for label in legend.get_lines():
+    #     label.set_linewidth(1.5)  # the legend line width
+    #
+    # plt.savefig("../similarities/Similarity_TOT_rangeB.png")
+    # plt.show()
+
+
+    # plt.plot((runtimes))
+    # plt.plot(np.diff(bins_sims))
+    # plt.plot(np.diff(runtimes))
     #
     # Testing for different b_lb values
-    for s in np.arange(0, 200,1):
-        # print s
-        similarity = (color_similarity(17, 18, 10, s))
-        # print similarity
-        blb_sims.append(similarity)
-    plt.plot((blb_sims))
-    plt.show()
+    # for s in np.arange(0, 10,0.1):
+    #     similarity_1 = (color_similarity(3, 4, 14, s))
+    #     similarity_2 = (color_similarity(5, 6, 14, s))
+    #     similarity_3 = (color_similarity(13, 14, 14, s))
+    #     similarity_4 = (color_similarity(17, 18, 14, s))
+    #     similarity_5 = (color_similarity(7, 9, 14, s))
+    #     similarity_6 = (color_similarity(29, 30, 14, s))
+    #
+    #     diff2 = similarity_1 - similarity_2
+    #     diff3 = similarity_1 - similarity_3
+    #     diff4 = similarity_1 - similarity_4
+    #     diff5 = similarity_1 - similarity_5
+    #     diff6 = similarity_1 - similarity_6
+    #
+    #     bins_sims_1.append(similarity_1)
+    #     bins_sims_2.append(similarity_2)
+    #     bins_sims_3.append(diff2)
+    #
+    #     # bins_sims_1.append((diff2))
+    #     # bins_sims_2.append((diff3))
+    #     # bins_sims_3.append((diff4))
+    #     # bins_sims_4.append((diff5))
+    #     # bins_sims_5.append((diff6))
+    #
+    # fig, ax = plt.subplots()
+    #
+    # g1 = "similarity(A)"
+    # g2 = "similarity(B)"
+    # g3 = "similarity(A) - similarity(B)"
+
+    # g1 = "similarity(A) - similarity(B)"
+    # g2 = "similarity(A) - similarity(C)"
+    # g3 = "similarity(A) - similarity(D)"
+    # g4 = "similarity(A) - similarity(E)"
+    # g5 = "similarity(A) - similarity(F)"
+    # g6 = "F"
+
+    # ax.plot((bins_sims_1), label=g1)
+    # ax.plot((bins_sims_2), label=g2)
+    # ax.plot((bins_sims_3), label=g3)
+    # ax.plot((bins_sims_4), label=g4)
+    # ax.plot((bins_sims_5), label=g5)
+
+    # legend = ax.legend(loc='upper right', shadow=False)
+    #
+    # frame = legend.get_frame()
+    # frame.set_facecolor('1.0')
+    #
+    # plt.xlabel("Number of Bins")
+    # plt.ylabel("Similarity")
+    #
+    # for label in legend.get_texts():
+    #     label.set_fontsize('large')
+    #
+    # for label in legend.get_lines():
+    #     label.set_linewidth(1.5)  # the legend line width
+    #
+    # plt.savefig("../similarities/Texture_Similarity_Fix_A.png")
+    # plt.show()
+
+
 
 
 
--- histogram.py
+++ histogram.py
@@ -1,23 +1,68 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
-def histogram(bin_size, idx):
-    if idx < 10: idx1 = "0" + str(idx)
+def histogram(nbins, idx, b_lb):
 
+    if idx < 10: idx = "0" + str(idx)
     img = cv2.imread("../Images/i" + str(idx) + ".ppm")
 
-    rows, cols, depth = img.shape
-    print rows, cols
+    # 3, 2D histograms
+    fig = plt.figure()
+    chans = cv2.split(img)
 
-    v = (256 / bin_size) + 1
-    hist = [np.zeros(v), np.zeros(v), np.zeros(v)]
+    lb = (b_lb, b_lb, b_lb)
+    ub = (255, 255, 255)
+    mask = cv2.inRange(img, lb, ub)
 
-    for r in xrange(0, rows):
-        for c in xrange(0, cols):
-            pixel = img[r,c]    # 3x1 array
-            for i in xrange(0,3):
-                pixel[i] = round(pixel[i]/bin_size)
-                hist[i][pixel[i]] += 1
+    # plot a 2D color histogram for green and blue
+    ax = fig.add_subplot(131)
+    hist = cv2.calcHist([chans[1], chans[0]], [0, 1], mask,
+        [nbins, nbins], [0, 256, 0, 256])
+    p = ax.imshow(hist, interpolation = "nearest")
+    ax.set_title("2D Color Histogram for Green and Blue")
+    plt.colorbar(p)
 
-    hist = np.array(hist).ravel()
-    return hist
+    # plot a 2D color histogram for green and red
+    ax = fig.add_subplot(132)
+    hist = cv2.calcHist([chans[1], chans[2]], [0, 1], mask,
+	[nbins, nbins], [0, 256, 0, 256])
+    p = ax.imshow(hist, interpolation = "nearest")
+    ax.set_title("2D Color Histogram for Green and Red")
+    plt.colorbar(p)
+
+    # plot a 2D color histogram for blue and red
+    ax = fig.add_subplot(133)
+    hist = cv2.calcHist([chans[0], chans[2]], [0, 1], mask,
+	[nbins, nbins], [0, 256, 0, 256])
+    p = ax.imshow(hist, interpolation = "nearest")
+    ax.set_title("2D Color Histogram for Blue and Red")
+    plt.colorbar(p)
+
+    # finally, let's examine the dimensionality of one of
+    # the 2D histograms
+    print "2D histogram shape: %s, with %d values" % (
+	hist.shape, hist.flatten().shape[0])
+
+    plt.show()
+
+
+    #
+    # rows, cols, depth = img.shape
+    # print rows, cols
+    #
+    # v = (256 / bin_size) + 1
+    # hist = [np.zeros(v), np.zeros(v), np.zeros(v)]
+    #
+    # for r in xrange(0, rows):
+    #     for c in xrange(0, cols):
+    #         pixel = img[r,c]    # 3x1 array
+    #         for i in xrange(0,3):
+    #             pixel[i] = round(pixel[i]/bin_size)
+    #             hist[i][pixel[i]] += 1
+    #
+    # hist = np.array(hist).ravel()
+    return True
+
+if __name__ == "__main__":
+    histogram(100, 5, 40)
